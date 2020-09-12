25082bdf12a7c0953793e4770058119bf9079649
gj <loritym@gmail.com>
2020-09-12 08:18:21 +0200

    version2

--- src/start/Start.java
+++ src/start/Start.java
@@ -3,5 +3,7 @@ package start;
 public class Start {
     public static void main(String[] args) {
         System.out.println("guten Morgen");
+
+        System.out.println("next version");
     }
 }
